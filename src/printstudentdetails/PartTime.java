1372690041348f9806a1305baf42a0d6551ba0ed
sivagamasrinivasan <784108@pdsb.net>
2023-09-25 14:58:50 -0400

    branch codeparttime added

---
+++ src/printstudentdetails/PartTime.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package printstudentdetails;
+
+/**
+ *
+ * @author kumar
+ */
+public class PartTime {
+    private int numofcourses;
+
+    /**
+     * @return the numofcourses
+     */
+    public int getNumofcourses() {
+        return numofcourses;
+    }
+
+    /**
+     * @param numofcourses the numofcourses to set
+     */
+    public void setNumofcourses(int numofcourses) {
+        this.numofcourses = numofcourses;
+    }
+}
